51a7243945716777f609f4355239eb686f6c3fc9
Malgorzata Ratajczyk <malgorzataratajczyk@wp.pl>
2018-09-24 21:49:41 +0200

    Add modules and app folders and create program.js and OSinfo.js files

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//ustawienia odpowiedniego enkodowania przyjmowanych danych
+process.stdin.setEncoding('utf-8');
+// ustawienie nasłuchiwania na zdarzenia odczytu
+process.stdin.on('readable', function() {
+    // treść tego co ma się wykonać w momencie odczytania wejścia  
+    var input = process.stdin.read(); // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
+    if(input !== null) { //sprawdzenie czy na wejściu cokolwiek istnieje
+        // teraz jest sens cokolwiek wyświetlać
+        var instruction = input.toString().trim();
+        switch (instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            case '/sayhello':
+                process.stdout.write('hello!\n');
+                break;
+            case '/ process.env':
+            process.stdout.write('\nNode.js Version\n')
+            process.stdout.write(process.version);
+            process.stdout.write('\nOS Version\n');
+            process.stdout.write(process.env.OS);
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
+        }
+    }
+});
