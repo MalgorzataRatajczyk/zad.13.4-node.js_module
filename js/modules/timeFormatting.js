b817c00193ab2c5cf77a8318650f06d2dcc631ac
Malgorzata Ratajczyk <malgorzataratajczyk@wp.pl>
2018-09-25 23:23:35 +0200

    Add console.log

--- js/modules/timeFormatting.js
+++ js/modules/timeFormatting.js
@@ -5,17 +5,18 @@ function timeFormat (seconds) {
         return seconds + ' sek.';
     } else { 
         if (seconds >= 60 && seconds <= 3600) {
-        var minute = Math.floor(seconds / 60);
-        var restSek = seconds - minute * 60;
+            var minute = Math.floor(seconds / 60);
+            var restSek = seconds - minute * 60;
         return minute + ' min. ' + restSek + ' sek.';
         }
         if (seconds > 3600) {
             var hour = Math.floor(seconds / 3600);
             var restMin = Math.floor((seconds - hour * 3600) / 60);
             var restSek = (seconds - (hour * 3600) - (restMin * 60));
-            return hour + ' h ' + restMin + ' min. ' + restSek + ' sek.';
+        return hour + ' h ' + restMin + ' min. ' + restSek + ' sek.';
         }
     }
 }
+console.log(timeFormat(seconds));
 
 exports.timeFormatting = timeFormat; //wyeksportowanie funkcji timeFormat
